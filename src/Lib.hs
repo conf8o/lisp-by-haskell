d77967e82b237e65e9f0c31ab3c2305b5cf0026c
conf8o <41929738+conf8o@users.noreply.github.com>
2025-05-24 12:19:46 +0900

    basic parts

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,44 @@
 module Lib
-    ( someFunc
-    ) where
+  ( someFunc,
+    Atom,
+    Symbol,
+    Special,
+    Cons,
+    Data,
+    eval,
+    evalCons,
+    plus,
+  )
+where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+data Atom = Integer Integer | Char Char | Bool Bool | String String | Fn (Cons -> Data) | Symbol Symbol
+
+newtype Symbol = Special Special
+
+data Special = If | Let | Quote
+
+type Cons = (Data, Data)
+
+data Data = Atom Atom | List Cons | Nil | Error String
+
+eval :: Data -> Data
+eval (Atom a) = Atom a
+eval Nil = Nil
+eval (Error e) = Error e
+eval (List (data1, data2)) =
+  case (data1, data2) of
+    (Atom (Fn f), List args) -> f $ evalCons args
+    (d1, d2) -> List (eval d1, eval d2)
+
+evalCons :: Cons -> Cons
+evalCons (d, Nil) = (d, Nil)
+evalCons (d, Atom a) = (d, Atom a)
+evalCons (d, Error e) = (d, Error e)
+evalCons (d, List cons) = (eval d, List $ evalCons cons)
+
+plus :: Cons -> Data
+plus (Atom (Integer i), Atom (Integer j)) = Atom $ Integer $ i + j
+plus _ = Error "plus Error"
--- src/S.hs
+++ src/S.hs
@@ -1,13 +1,30 @@
-data Atom = Integer Integer | Char Char | Bool Bool | String String | Fn (List Atom -> Obj) | Symbol Symbol
+module S (Atom, Symbol, Special, Cons, Data, eval, evalCons, plus) where
 
-data Symbol = Special Special | Key Key
+data Atom = Integer Integer | Char Char | Bool Bool | String String | Fn (Cons -> Data) | Symbol Symbol
 
-type Key = String
+newtype Symbol = Special Special
 
 data Special = If | Let | Quote
 
-data List a = Cons a a
+type Cons = (Data, Data)
 
-data Obj = Atom Atom | List (List Obj) | Nil
+data Data = Atom Atom | List Cons | Nil | Error String
 
-type Env = [(Key, Obj)]
+eval :: Data -> Data
+eval (Atom a) = Atom a
+eval Nil = Nil
+eval (Error e) = Error e
+eval (List (data1, data2)) =
+  case (data1, data2) of
+    (Atom (Fn f), List args) -> f $ evalCons args
+    (d1, d2) -> List (eval d1, eval d2)
+
+evalCons :: Cons -> Cons
+evalCons (d, Nil) = (d, Nil)
+evalCons (d, Atom a) = (d, Atom a)
+evalCons (d, Error e) = (d, Error e)
+evalCons (d, List cons) = (eval d, List $ evalCons cons)
+
+plus :: Cons -> Data
+plus (Atom (Integer i), Atom (Integer j)) = Atom $ Integer $ i + j
+plus _ = Error "plus Error"
