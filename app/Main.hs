a1f410faf1a45904b820f9c5e050731c1ca55eb2
conf8o <41929738+conf8o@users.noreply.github.com>
2025-05-28 21:27:58 +0900

    remove atom

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,21 @@
 module Main (main) where
 
 import Lib
+import qualified S
 
 main :: IO ()
-main = someFunc
+main = do
+  case sPlus of
+    (S.Value (S.Integer 3)) -> putStrLn "3"
+    _ -> putStrLn ""
+
+sPlus :: S.Data
+sPlus =
+  S.eval $
+    S.List
+      ( S.Fn S.plus,
+        S.List
+          ( S.Value (S.Integer 1),
+            S.Value (S.Integer 2)
+          )
+      )
--- src/S.hs
+++ src/S.hs
@@ -1,30 +1,26 @@
-module S (Atom, Symbol, Special, Cons, Data, eval, evalCons, plus) where
+module S (eval, plus, Value (..), Special (..), Cons, Data (..)) where
 
-data Atom = Integer Integer | Char Char | Bool Bool | String String | Fn (Cons -> Data) | Symbol Symbol
-
-newtype Symbol = Special Special
+data Value = Integer Integer | Char Char | Bool Bool | String String
 
 data Special = If | Let | Quote
 
 type Cons = (Data, Data)
 
-data Data = Atom Atom | List Cons | Nil | Error String
+data Data = Value Value | List Cons | Fn (Cons -> Data) | Symbol String | Special Special | Error String
 
 eval :: Data -> Data
-eval (Atom a) = Atom a
-eval Nil = Nil
-eval (Error e) = Error e
 eval (List (data1, data2)) =
   case (data1, data2) of
-    (Atom (Fn f), List args) -> f $ evalCons args
+    (Fn f, List args) -> f $ evalCons args
     (d1, d2) -> List (eval d1, eval d2)
+eval (Symbol _) =
+  Error "TODO: 環境を実装する"
+eval x = x
 
 evalCons :: Cons -> Cons
-evalCons (d, Nil) = (d, Nil)
-evalCons (d, Atom a) = (d, Atom a)
-evalCons (d, Error e) = (d, Error e)
-evalCons (d, List cons) = (eval d, List $ evalCons cons)
+evalCons (x, List xs) = (eval x, List $ evalCons xs)
+evalCons (x, y) = (eval x, eval y)
 
 plus :: Cons -> Data
-plus (Atom (Integer i), Atom (Integer j)) = Atom $ Integer $ i + j
+plus (Value (Integer i), Value (Integer j)) = Value $ Integer $ i + j
 plus _ = Error "plus Error"
